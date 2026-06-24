596020c9936ce927985f25e44e614a03a61a3079
kalyankorada16-arch <kalyankorada16@gmail.com>
2026-06-24 09:32:43 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,70 @@
+'''
+                                      DAY 2 - PYTHON OPERATORS  
+
+Type of operators:
+
+1.Arthimetuc operator: It is used for Mathematical operations.
+    EX: +, -, *, %, /, //, **
+    a = 20
+    b = 5
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a**b)
+    print(a%b)
+    print(a//b)
+
+2.Assignment operator: Assignment operators are used to assign values to variables and update their values.
+    EX: =, +=, -=, *=, %=
+    a = 20
+    a+=4 #a = a+4
+    print(a)
+    a-=4
+    print(a)
+    a*=4
+    print(a)
+
+3.Comparison  operator: Comparison operators are used to compare two values and return either True or False.
+    EX: ==, <=, >=, !=
+    a = 20
+    b = 7
+    print(a==b)
+    print(a!=b)
+    print(a<=b)
+    print(a>=b)
+    
+4.Logical Operator: Logical operators are used to combine multiple conditions and return True or False
+    EX: and ,or ,not
+    a = 20
+    b = 7
+    c = 89
+    print( a<=b and b<=c) # True(20<=7 and 7<=89)
+    print( a<=b or b>=c) # True(20<=7 or 7>=89)
+    not - reverser the output
+    
+5.Identity operator: Identity operators are used to check whether two variables refer to the same object in memory.
+    EX: is, is not
+    a = 20
+    b = 20
+    print( a==b )
+    print( a is b )
+    print ( a is not b )
+    
+6.Membership operator: Membership operators are used to check whether a value is present in a sequence (such as a string, list, tuple, or set).
+    EX: in, not in
+    a = [1,2,3,4,5]
+    print( 2 in a )
+    print( 20 not in a )
+    
+7.Bitwise operator: Bitwise operators perform operations on the binary (bit-level) representation of integers.
+    EX: <<, >>, |, ^
+    print( 5&4 )
+    print( 5|4 )
+    print(<<5)
+    print(5>>)
+
+
+
+'''
+
