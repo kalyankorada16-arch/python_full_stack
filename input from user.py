94a798695f435f25039fae25cdcc9a3b7684b8be
kalyankorada16-arch <kalyankorada16@gmail.com>
2026-06-30 12:37:58 +0530

    Add files via upload

---
+++ input from user.py
@@ -0,0 +1,66 @@
+'''
+
+          Input formatting from user
+
+    1.input()
+    -------------
+    The input() fuction is used to take from the user...
+    1.int
+    ------------
+    num = int(input("enter a number: "))
+    num_2 = int(inpu("enter a number: "))
+    print(num + num_2)
+
+    2.string
+    ------------
+    eg-
+    how = input("enter a char: ")
+    print(how + 'hero')
+
+    3.float
+    ------------
+    eg-
+    num = float(input("enter your salary: "))
+    print(num, "is the monthly salary: "))
+
+    4.list
+    ------------
+    eg-
+    group_ = list(map(int,input().split()))
+    print(group_)
+
+    5.tuple
+    ------------
+    some = tuple(map(int,input().split()))
+    print(some)
+    group_ = tuple(input().split())
+    print(group_)
+
+    num = eval(input('enter: ')
+    print(type(num))
+    
+
+    name = input('enter your name: ')
+   age = int(input('enter your age: '))
+   print(name,'your age is ',age)
+
+   name = input("enter your name
+   age = int(input("enter your age: "))
+   print("my name is %s and i'm %d years old" %(name , age))
+
+
+
+
+
+
+
+
+
+    '''
+
+
+
+
+
+
+
